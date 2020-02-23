21945a139154675346f5051737109059457a27e4
BVRIT <BVRIT@PLACEMENT-LAB-30>
2020-02-23 11:53:17 +0530

    commit

---
+++ Java-8Features/src/com/java8/datetime/LocalTime.java
@@ -0,0 +1,17 @@
+package com.java8.datetime;
+
+import java.time.LocalDateTime;
+
+public class LocalTime {
+
+	public static void main(String[] args) {
+		LocalDateTime localDateTime=LocalDateTime.now();
+		System.out.println(localDateTime.toString());
+		System.out.println(localDateTime.getDayOfMonth());
+		System.out.println(localDateTime.getHour());
+		System.out.println(localDateTime.getNano());
+		
+
+	}
+
+}
